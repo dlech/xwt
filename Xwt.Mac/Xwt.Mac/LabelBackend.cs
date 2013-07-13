dlech/xwt

commit e7586f3e35c1250116015a14547eccd18e3d1fd4

[Mac] Vertically align labels and single-line text entries

Xwt.Mac/Xwt.Mac/LabelBackend.cs
@@ -28,13 +28,14 @@ using System;
 using MonoMac.AppKit;
 using Xwt.Backends;
 using MonoMac.Foundation;
+using MonoMac.ObjCRuntime;
 
 namespace Xwt.Mac
 {
-	public class LabelBackend: ViewBackend<NSTextField,IWidgetEventSink>, ILabelBackend
+	public class LabelBackend: ViewBackend<NSView,IWidgetEventSink>, ILabelBackend
 	{
 		public LabelBackend ()
-			: this (new TextFieldView ())
+			: this (new CustomAlignedContainer (new TextFieldView ()))
 		{
 		}
 
@@ -44,6 +45,19 @@ namespace Xwt.Mac
 			Widget.Editable = false;
 			Widget.Bezeled = false;
 			Widget.DrawsBackground = false;
+		}
+
+		protected override void OnSizeToFit ()
+		{
+			Container.SizeToFit ();
+		}
+
+		CustomAlignedContainer Container {
+			get { return (CustomAlignedContainer)base.Widget; }
+		}
+
+		public new NSTextField Widget {
+			get { return (NSTextField) Container.Child; }
 		}
 
 		public virtual string Text {
@@ -133,6 +147,60 @@ namespace Xwt.Mac
 					}
 				}
 			}
+		}
+	}
+
+	sealed class CustomAlignedContainer: NSView, IViewObject
+	{
+		public NSView Child;
+
+		public CustomAlignedContainer (NSView child)
+		{
+			Child = child;
+			AddSubview (child);
+			UpdateTextFieldFrame ();
+		}
+
+		public ViewBackend Backend { get; set; }
+
+		public NSView View {
+			get { return this; }
+		}
+
+		static readonly Selector sizeToFitSel = new Selector ("sizeToFit");
+
+		public void SizeToFit ()
+		{
+			if (Child.RespondsToSelector (sizeToFitSel))
+				Messaging.void_objc_msgSend (Child.Handle, sizeToFitSel.Handle);
+			else
+				throw new NotSupportedException ();
+			Frame = new System.Drawing.RectangleF (Frame.X, Frame.Y, Child.Frame.Width, Child.Frame.Height);
+		}
+
+		bool expandVertically;
+		public bool ExpandVertically {
+			get {
+				return expandVertically;
+			}
+			set {
+				expandVertically = value;
+				UpdateTextFieldFrame ();
+			}
+		}
+
+		public override void SetFrameSize (System.Drawing.SizeF newSize)
+		{
+			base.SetFrameSize (newSize);
+			UpdateTextFieldFrame ();
+		}
+
+		void UpdateTextFieldFrame ()
+		{
+			if (expandVertically)
+				Child.Frame = Frame;
+			else
+				Child.Frame = new System.Drawing.RectangleF (0, (Frame.Height - Child.Frame.Height) / 2, Frame.Width, Child.Frame.Height);
 		}
 	}
 	

Xwt.Mac/Xwt.Mac/TextEntryBackend.cs
@@ -30,7 +30,7 @@ using MonoMac.AppKit;
 
 namespace Xwt.Mac
 {
-	public class TextEntryBackend: ViewBackend<NSTextField,ITextEntryEventSink>, ITextEntryBackend
+	public class TextEntryBackend: ViewBackend<NSView,ITextEntryEventSink>, ITextEntryBackend
 	{
 		public TextEntryBackend ()
 		{
@@ -47,11 +47,24 @@ namespace Xwt.Mac
 			if (ViewObject is MacComboBox) {
 				((MacComboBox)ViewObject).SetEntryEventSink (EventSink);
 			} else {
-				ViewObject = new CustomTextField (EventSink, ApplicationContext);
+				ViewObject = new CustomAlignedContainer (new CustomTextField (EventSink, ApplicationContext));
 			}
 			MultiLine = false;
 		}
 		
+		protected override void OnSizeToFit ()
+		{
+			Container.SizeToFit ();
+		}
+
+		CustomAlignedContainer Container {
+			get { return (CustomAlignedContainer)base.Widget; }
+		}
+
+		public new NSTextField Widget {
+			get { return (NSTextField) Container.Child; }
+		}
+
 		protected override Size GetNaturalSize ()
 		{
 			var s = base.GetNaturalSize ();
@@ -110,14 +123,15 @@ namespace Xwt.Mac
 			}
 			set {
 				if (value) {
-					Widget.Cell.UsesSingleLineMode = true;
-					Widget.Cell.Scrollable = true;
-					Widget.Cell.Wraps = false;
-				} else {
 					Widget.Cell.UsesSingleLineMode = false;
 					Widget.Cell.Scrollable = false;
 					Widget.Cell.Wraps = true;
+				} else {
+					Widget.Cell.UsesSingleLineMode = true;
+					Widget.Cell.Scrollable = true;
+					Widget.Cell.Wraps = false;
 				}
+				Container.ExpandVertically = value;
 			}
 		}
 

commit 90a1c11571c2186652075c2e1055267e07fed0df

[Mac] fix link label breakage after e7586f3e35c1250116015a14547eccd18e3d1fd4

Xwt.Mac/Xwt.Mac/LabelBackend.cs
@@ -35,13 +35,13 @@ namespace Xwt.Mac
 	public class LabelBackend: ViewBackend<NSView,IWidgetEventSink>, ILabelBackend
 	{
 		public LabelBackend ()
-			: this (new CustomAlignedContainer (new TextFieldView ()))
+			: this (new TextFieldView ())
 		{
 		}
 
 		protected LabelBackend (IViewObject viewObject)
 		{
-			ViewObject = viewObject;
+			ViewObject = new CustomAlignedContainer (viewObject);
 			Widget.Editable = false;
 			Widget.Bezeled = false;
 			Widget.DrawsBackground = false;
@@ -154,10 +154,10 @@ namespace Xwt.Mac
 	{
 		public NSView Child;
 
-		public CustomAlignedContainer (NSView child)
+		public CustomAlignedContainer (IViewObject child)
 		{
-			Child = child;
-			AddSubview (child);
+			Child = child.View;
+			AddSubview (Child);
 			UpdateTextFieldFrame ();
 		}
 